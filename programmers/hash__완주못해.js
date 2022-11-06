c2df11eb66bde44e7f369e4c8a7dc261e4ffb1c5
teetee6 <teetee6@naver.com>
2022-11-06 23:44:56 +0900

    test

---
+++ programmers/hash__완주못해.js
@@ -0,0 +1,12 @@
+// function comp_str(a, b) {
+//     return a.localeCompare(b);
+// }
+
+function solution(participant, completion) {
+    participant.sort();
+    completion.sort();
+
+    for(let i in participant) {
+        if(participant[i] !== completion[i]) return participant[i];
+    }
+}
