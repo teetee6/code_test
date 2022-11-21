3f4f31b00a2e69628d09323b606c75a90c3d0597
teetee6 <teetee6@naver.com>
2022-11-22 02:49:33 +0900

    dfs

--- cps/59__subset(dfs).js
+++ cps/59__subset(dfs).js
@@ -32,15 +32,48 @@ const rl = readline.createInterface({
     // }
     // dfs(1, []);
 
-    function dfs(lv, arr) {
-      if (lv === n + 1) return;
-      for (let i = lv; i <= n; ++i) {
-        arr.push(i);
-        dfs(i + 1, arr);
-        console.log(arr.reduce((acc, cur) => acc + cur + " ", "").trimEnd());
-        arr.pop();
+    let arr = new Array(100).fill(0);
+    function dfs(lv) {
+      if (lv === n + 1) {
+        let res = "";
+        for (let i = 1; i < lv; ++i) {
+          arr[i] === 1 ? (res += i + " ") : null;
+        }
+        process.stdout.write(res.trimEnd() + "\n");
+        return;
       }
+      arr[lv] = 1;
+      dfs(lv + 1);
+      arr[lv] = 0;
+      dfs(lv + 1);
     }
-    dfs(1, []);
+    dfs(1);
+
+    // let stack = [[1, ""]];
+    // while (stack.length) {
+    //   let cur = stack.pop();
+    //   if (cur[0] === n + 1) {
+    //     const arr = cur[1].trimEnd().split(" ");
+    //     let res = "";
+    //     for (let i = 1; i <= n; ++i) {
+    //       arr[i - 1] === "1" ? (res += i + " ") : null;
+    //     }
+    //     process.stdout.write(res.trimEnd() + "\n");
+    //     continue;
+    //   }
+    //   stack.push([cur[0] + 1, cur[1] + "0 "]);
+    //   stack.push([cur[0] + 1, cur[1] + "1 "]);
+    // }
+
+    // let stack = [[1, ""]];
+    // while (stack.length) {
+    //   let cur = stack.pop();
+    //   if (cur[0] === n + 1) {
+    //     console.log(cur[1]);
+    //     continue;
+    //   }
+    //   stack.push([cur[0] + 1, cur[1]]);
+    //   stack.push([cur[0] + 1, cur[1] + cur[0] + " "]);
+    // }
   });
 })();
