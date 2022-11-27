fa3b389a388d27030e34e1537551a91846228b1a
teetee6 <teetee6@naver.com>
2022-11-27 11:58:41 +0900

    바꿔꿔

--- acmicpc/15591.js
+++ acmicpc/15591.js
@@ -4,52 +4,60 @@ const rl = readline.createInterface({
   crlfDelay: Infinity,
 });
 
-(() => {
+(function main() {
   let graph,
     n,
     q,
     i = -1;
 
   let answer = [];
-  rl.on("line", (line) => {
+  rl.on("line", function evLstn(line) {
     let input = line
       .trim()
       .split(" ")
+      .filter((v) => v !== "")
       .map((v) => parseInt.call(null, v, 10));
     if (i == -1) {
-      [n, q] = [input[0], input[1]];
-      graph = Array.from({ length: n }, (v) =>
-        Array.from({ length: n }, (v) => Infinity)
-      );
+      [n, q] = [...input];
+      graph = Array.from({ length: n }, () => []);
       ++i;
       return;
     }
     if (i < n - 1) {
-      graph[input[0] - 1][input[1] - 1] = input[2];
-      graph[input[1] - 1][input[0] - 1] = input[2];
+      graph[input[0] - 1].push([input[1] - 1, input[2]]);
+      graph[input[1] - 1].push([input[0] - 1, input[2]]);
       ++i;
-      if (i === n - 1) algo();
       return;
     } else if (i < n - 1 + q) {
       let [k, v] = [input[0], input[1]];
-      let cnt = 0;
-      for (let j = 1; j <= n; ++j) {
-        graph[v - 1][j - 1] >= k ? ++cnt : undefined;
-      }
-      answer.push(cnt - 1);
+      algo(k, v);
       ++i;
     }
     if (i === n - 1 + q) {
       rl.close();
+      rl.removeListener("line", evLstn);
       return;
     }
   }).on("close", () => {
     answer.forEach((v) => console.log(v));
   });
 
-  function algo() {
-    console.log(graph);
-    for (let i = 1; i <= n; ++i) {}
-    console.log(graph);
+  function dfs(k, v) {}
+
+  function algo(k, v) {
+    dfs(k, v);
+
+    // while (stack.length) {
+    //   const cur = stack.pop();
+    //   if (visited[cur[0]]) continue;
+    //   visited[cur[0]] = true;
+    //   cur[1] !== -1 && cur[1] >= k
+    //     ? (++cnt, (min = Math.min(min, cur[1])), min === cur[1] ? )
+    //     : null;
+    //   for (let elem of graph[cur[0]]) {
+    //     stack.push(elem);
+    //   }
+    // }
+    console.log(cnt);
   }
 })();
